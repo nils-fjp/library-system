8329eb0844a475caa5f79e94175eab3cc403b06c
nils-fjp <nils.fj.paulsson@outlook.com>
2026-03-25 16:30:45 +0100

    add Menu class and ANSI class as an example

---
+++ src/ANSI.java
@@ -0,0 +1,41 @@
+@SuppressWarnings("unused")
+public class ANSI {
+    // https://en.wikipedia.org/wiki/ANSI_escape_code#Select_Graphic_Rendition_parameters
+    // ANSI escape sequences for styling terminal output
+
+    public static final String RESET = "\033[0m";
+    public static final String BOLD = "\033[1m";
+    public static final String FAINT = "\033[2m";
+    public static final String ITALIC = "\033[3m";
+    public static final String UNDERLINE = "\033[4m";
+    public static final String SLOW_BLINK = "\033[5m";
+
+    public static final String NO_BOLD = "\033[22m";
+    public static final String NO_ITALIC = "\033[23m";
+    public static final String NO_UNDERLINE = "\033[24m";
+    public static final String NO_BLINK = "\033[25m";
+
+    public static final String RED = "\033[31m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String BLUE = "\033[34m";
+    public static final String MAGENTA = "\033[35m";
+    public static final String CYAN = "\033[36m";
+    public static final String WHITE = "\033[37m";
+
+    public static final String ORANGE = "\033[38;5;166m";
+    public static final String DEFAULT_FG = "\033[39m";
+
+    public static final String BRIGHT_BLACK = "\033[90m";
+    public static final String BRIGHT_RED = "\033[91m";
+    public static final String BRIGHT_GREEN = "\033[92m";
+    public static final String BRIGHT_YELLOW = "\033[93m";
+    public static final String BRIGHT_BLUE = "\033[94m";
+    public static final String BRIGHT_MAGENTA = "\033[95m";
+    public static final String BRIGHT_CYAN = "\033[96m";
+    public static final String BRIGHT_WHITE = "\033[97m";
+
+    public static final String CLEAR_SCREEN = "\033[2J\033[H";
+    public static final String HIDE_CURSOR = "\033[?25l";
+    public static final String SHOW_CURSOR = "\033[?25h";
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,43 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Skapa en ny meny för att visa information
+        Menu mainMenu = new Menu();
+
+        // Välj vad det ska stå överst i menyn
+        mainMenu.setTitle("Main Menu");
+        mainMenu.setSubtitle("Subtitle");
+        mainMenu.setMenuInfo("Menu Info");
+        mainMenu.setExitOption("Exit");
+        mainMenu.addMenuOption("Option 1");
+        mainMenu.addMenuOption("Option 2");
+        mainMenu.addMenuOption("Option 3");
+        mainMenu.addMenuOption("Option 4");
+        mainMenu.setPrePrompt("Type a number and press enter...");
+        mainMenu.setPromptLine("Enter: ");
+        while (mainMenu.showMenu()) {
+
+            switch (mainMenu.getChoice()) {
+                case 0: {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                case 1: {
+                    System.out.println("Option 1 not implemented yet");
+                    break;
+                }
+                case 2: {
+                    System.out.println("Cption 2 implemented yet");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Option 3 not implemented yet");
+                    break;
+                }
+                default:
+                    System.out.println("Invalid Input");
+            }
         }
     }
 }
