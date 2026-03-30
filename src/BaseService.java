55a39aac9f1b94e8d6c3989fe6b18f7ba261f63e
Olena Vitkovska <olena.vitkovska86@gmail.com>
2026-03-30 15:31:23 +0200

    make changes

--- src/BaseService.java
+++ src/BaseService.java
@@ -1,3 +1,44 @@
-public class BaseService {
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
+public abstract class BaseService<T, ID> {
+
+    protected abstract BaseRepository<T, ID> getRepository();
+
+    public Optional<T> getById(ID id) throws SQLException {
+        validateId(id);
+        return getRepository().getById(id);
+    }
+
+    public List<T> getAll() throws SQLException {
+        return getRepository().getAll();
+    }
+
+    public void save(T entity) throws SQLException {
+        validate(entity);
+        getRepository().save(entity);
+    }
+
+    public void update(T entity) throws SQLException {
+        validate(entity);
+        getRepository().update(entity);
+    }
+
+    public void deleteById(ID id) throws SQLException {
+        validateId(id);
+        getRepository().deleteById(id);
+    }
+
+    protected void validate(T entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException("entity must not be null");
+        }
+    }
+
+    protected void validateId(ID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("id must not be null");
+        }
+    }
 }
