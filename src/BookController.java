37643f414cc96b31a917c4d7b930d29e63052093
nils-fjp <nils.fj.paulsson@outlook.com>
2026-03-30 13:20:14 +0200

    add generic types to BaseController and -Service

--- src/BookController.java
+++ src/BookController.java
@@ -5,8 +5,8 @@ public class BookController extends BaseController {
 
         Menu bookMenu = new Menu();
 
-        bookMenu.setTitle("Book Menu");
-        // bookMenu.setSubtitle("Subtitle");
+        bookMenu.setTopTitle("Main Menu » Book Menu");
+        bookMenu.setMidTitle("Book Menu");
         // bookMenu.setMenuInfo("Menu Info");
         bookMenu.setExitOption("Back to Menu");
         bookMenu.addMenuOption("Search all books");
@@ -19,7 +19,6 @@ public class BookController extends BaseController {
             switch (bookMenu.getChoice()) {
                 case 0: {
                     break;
-
                 }
                 case 1: {
                     System.out.println("WIP!");
--- src/BookRepository.java
+++ src/BookRepository.java
@@ -1,14 +1,10 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookRepository extends BaseRepository {
-    /**
-     * @param id
-     * @return
-     * @throws SQLException
-     */
     @Override
     public Object getById(Object id) throws SQLException {
 
@@ -16,35 +12,35 @@ public class BookRepository extends BaseRepository {
         try (
             Connection connection = getConnection();
             java.sql.Statement statement = connection.createStatement()
-            ) {
-                ResultSet resultSet = statement.executeQuery(
-                    "SELECT * FROM books WHERE books.id = " + id
-                );
-                while (resultSet.next()) {
-                    String title = resultSet.getString("title");
-                    int yearPublished = resultSet.getInt("year_published");
-                    int availableCopies = resultSet.getInt("available_copies");
-                    String summary = resultSet.getString("summary");
-                    String language = resultSet.getString("language");
-                    int pageCount = resultSet.getInt("page_count");
-                    String author = resultSet.getString("author");
+        ) {
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT * FROM books WHERE books.id = " + id
+                                                        );
+            while (resultSet.next()) {
+                String title = resultSet.getString("title");
+                int yearPublished = resultSet.getInt("year_published");
+                int availableCopies = resultSet.getInt("available_copies");
+                String summary = resultSet.getString("summary");
+                String language = resultSet.getString("language");
+                int pageCount = resultSet.getInt("page_count");
+                String author = resultSet.getString("author");
 
-                    book = new Book(title, yearPublished, availableCopies, summary, language, pageCount, author);
-                }
-            } catch (SQLException e) {
-                System.out.println("Error : " + e.getMessage());
+                book = new Book(title, yearPublished, availableCopies, summary, language, pageCount, author);
             }
-            return book;
+        } catch (SQLException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+        return book;
     }
 
     @Override
-    public List getAll() {
-        return List.of();
+    public ArrayList getAll() {
+        return (ArrayList) List.of();
     }
 
     @Override
-    public List search(String searchTerm) throws SQLException {
-        return List.of();
+    public ArrayList search(String searchTerm) throws SQLException {
+        return (ArrayList) List.of();
     }
 
 }
