2af13fff25a35ea7f63879b9eb7e91c0d1d9ebd7
nils-fjp <nils.fj.paulsson@outlook.com>
2026-03-30 16:24:36 +0200

    restore shelved changes

--- src/Menu.java
+++ src/Menu.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
-    private static final int BORDER = 48;
+    private static final int BORDER = 36;
     private static final int PADDING = BORDER - 4;
     private static final Scanner scanner = new Scanner(System.in);
     private final ArrayList<String> menuOptions;
     private int choice;
-    private String title;
-    private String subtitle;
+    private String topTitle;
+    private String midTitle;
     private String menuInfo;
     private String exitOption;
     private String prePrompt;
     private String promptLine;
 
     public Menu() {
-        this.title = "";
-        this.subtitle = "";
+        this.topTitle = "";
+        this.midTitle = "";
         this.menuInfo = "";
         this.exitOption = "Exit";
         this.menuOptions = new ArrayList<>();
@@ -30,12 +30,12 @@ public class Menu {
         return choice;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setTopTitle(String topTitle) {
+        this.topTitle = topTitle;
     }
 
-    public void setSubtitle(String subtitle) {
-        this.subtitle = subtitle;
+    public void setMidTitle(String midTitle) {
+        this.midTitle = midTitle;
     }
 
     public void setMenuInfo(String menuInfo) {
@@ -59,78 +59,77 @@ public class Menu {
         this.promptLine = promptLine;
     }
 
-    private void drawMenuHeader() {
+    public void drawMenuHeader() {
         // Klassen ANSI innehåller olika typer av formatering. Varje ANSI-property är en vanlig String.
         System.out.print(ANSI.CLEAR_SCREEN);
 
-        String headerTitle = title;
-        String titleFlex = "─".repeat(PADDING);
+        String headerTop = topTitle;
+        String topFlex = "─".repeat(PADDING);
 
-        if (title.isEmpty()) {
-            headerTitle = "─";
+        if (topTitle.isEmpty()) {
+            headerTop = "─" + topFlex;
         } else {
-            titleFlex = "─".repeat(PADDING - headerTitle.length() - 1);
-            headerTitle = " " + ANSI.BOLD + title + ANSI.NO_BOLD + " ";
+            topFlex = "─".repeat(PADDING - headerTop.length() - 1);
+            headerTop = " " + ANSI.BOLD + topTitle + ANSI.NO_BOLD + " ";
         }
         System.out.println(
-            "  ╭─" + headerTitle + titleFlex + "╮");
+            "\t╭─" + headerTop + topFlex + "╮");
 
-        if (!subtitle.isEmpty()) {
-            String centerFlex = " ".repeat((PADDING - subtitle.length()) / 2);
+        if (!midTitle.isEmpty()) {
+            String centerFlex = " ".repeat((PADDING - midTitle.length()) / 2);
+            String flexRest = " ".repeat((PADDING - midTitle.length()) % 2);
             System.out.println(
-                "  │ " + centerFlex + subtitle + centerFlex + " │"
+                "\t│ " + centerFlex + midTitle + centerFlex + flexRest + " │"
             );
         }
         System.out.println(
-            "  ╰──" + "─".repeat(PADDING) + "╯"
+            "\t╰──" + "─".repeat(PADDING) + "╯"
         );
     }
 
     // Visar information under menyns titel. Man behöver inte ha någon information om man inte vill.
-    private void drawMenuInfo() {
+    public void drawMenuInfo() {
         if (!menuInfo.isEmpty()) {
-
-            System.out.println("  " + menuInfo);
+            System.out.println("\t " + menuInfo);
         }
     }
 
     // Motsvarar raderna 19-25 i Nils BookController.showBookMenu()
-    private void drawMenuOptions() {
+    public void drawMenuOptions() {
         System.out.println(
-            "  ╭──" + "─".repeat(PADDING) + "╮"
+            "\t╭──" + "─".repeat(PADDING) + "╮"
         );
 
         for (int i = 1; i < menuOptions.size(); i++) {
             String menuOption = menuOptions.get(i);
             String optionFlex = " ".repeat(PADDING - (menuOption.length() + 3));
             System.out.println(
-                "  │  " + "\033[1m" + i + ". " + ANSI.DEFAULT_FG + menuOption + optionFlex + "│");
+                "\t│  " + ANSI.YELLOW + i + ". " + ANSI.DEFAULT_FG + menuOption + optionFlex + "│");
         }
-        System.out.println("  ├──" + "─".repeat(PADDING) + "┤");
+        System.out.println("\t├──" + "─".repeat(PADDING) + "┤");
 
         String exitOptionFlex = " ".repeat(PADDING - (exitOption.length() + 3));
         System.out.println(
-            "  │  " + ANSI.BRIGHT_BLACK + 0 + ". " + ANSI.DEFAULT_FG + exitOption + exitOptionFlex + "│");
-        System.out.println("  ╰──" + "─".repeat(PADDING) + "╯");
+            "\t│  " + ANSI.BRIGHT_BLACK + 0 + ". " + ANSI.DEFAULT_FG + exitOption + exitOptionFlex + "│");
+        System.out.println("\t╰──" + "─".repeat(PADDING) + "╯");
     }
 
     // Visar tips strax ovanför raden där man skriver sitt val
-    private void drawPrePrompt() {
+    public void drawPrePrompt() {
         System.out.println(
-            ANSI.BRIGHT_BLACK + "  " + prePrompt + ANSI.DEFAULT_FG);
+            ANSI.BRIGHT_BLACK + ANSI.ITALIC + "\t " + prePrompt + ANSI.NO_ITALIC + ANSI.DEFAULT_FG);
     }
 
     // Visar den text som finns på samma rad man skriver på. Typ, "Enter:"
-    private void drawPromptLine() {
+    public void drawPromptLine() {
         System.out.print(
-            ANSI.BRIGHT_BLACK + "  " + promptLine + ANSI.DEFAULT_FG);
+            "\t " + promptLine
+        );
     }
 
     // Baseras på Nils Main och BookController-menyer
     public boolean showMenu() {
-        boolean showing = true;
-        while (showing) {
-
+        while (true) {
             drawMenuHeader();
             drawMenuInfo();
             drawMenuOptions();
@@ -141,18 +140,13 @@ public class Menu {
             try {
                 input = Integer.parseInt(scanner.nextLine().trim());
             } catch (NumberFormatException e) {
-                continue;
-            }
-
-            if (input == 0) {
-                choice = input;
                 return false;
             }
             if (input > 0 && input < this.menuOptions.size()) {
                 choice = input;
                 return true;
             }
+            return false;
         }
-        return showing;
     }
 }
