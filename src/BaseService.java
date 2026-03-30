be453d54c08180a837b5ca5695325a5b191f8862
nils-fjp <nils.fj.paulsson@outlook.com>
2026-03-30 13:25:16 +0200

    clean up spacing and formatting

--- src/BaseService.java
+++ src/BaseService.java
@@ -1,7 +1,7 @@
 import java.sql.SQLException;
 import java.util.List;
 
-public abstract class BaseService<T,ID> {
+public abstract class BaseService<T, ID> {
     public abstract T getById(ID id) throws SQLException;
 
     public abstract Object getById(int id) throws SQLException;
