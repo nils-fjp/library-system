dec0bbbc74a35d54962f7d2335c5dd68de5e2dc1
nils-fjp <nils.fj.paulsson@outlook.com>
2026-03-30 13:20:14 +0200

    add generic types to BaseController and -Service

--- src/BaseService.java
+++ src/BaseService.java
@@ -1,11 +1,12 @@
 import java.sql.SQLException;
 import java.util.List;
 
-public abstract class BaseService {
-    public abstract Book getById(ID id) throws SQLException;
+public abstract class BaseService<T,ID> {
+    public abstract T getById(ID id) throws SQLException;
+
+    public abstract Object getById(int id) throws SQLException;
 
     public abstract List<T> getAll();
 
     public abstract List<T> search(String searchTerm) throws SQLException;
-}
 }
