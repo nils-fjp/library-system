9cb140b58cf1666428ec175f8890b08c1c13e737
Olena Vitkovska <olena.vitkovska86@gmail.com>
2026-04-01 16:38:31 +0200

    addnew menuControllers and create Member classes

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,87 @@
-import java.sql.SQLException;
-import java.util.Optional;
+//import java.sql.SQLException;
+//import java.util.Optional;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) throws SQLException {
+//
+//
+//
+//        // skapa meny som visar information och läser input
+//        Menu mainMenu = new Menu();
+//
+//        // Välj vad det ska stå i menytexten med följande metoder:
+//        mainMenu.setTopTitle("Main Menu");
+//        mainMenu.setMidTitle("Subtitle");
+//        mainMenu.setMenuInfo("Menu Info");
+//        mainMenu.setExitOption("Exit");
+//        mainMenu.addMenuOption("Show Book Menu");
+//        mainMenu.addMenuOption("Show Loan Menu");
+//        mainMenu.addMenuOption("Show Member Menu");
+//        mainMenu.setPrePrompt("Type a number and press enter...");
+//        mainMenu.setPromptLine("Enter: ");
+//
+//
+//        // Visa menyn genom showMenu() som returnerar en boolean.
+//        while (mainMenu.showMenu()) {
+//            // Läs in valet med en getter i meny-klassen.
+//            // Menyn läser bara input. Logiken måste hanteras med en switch.
+//            switch (mainMenu.getChoice()) {
+//                case 0: {
+//                    break; // Avslutar while-loopen (lämnar menyn)
+//                }
+//                case 1: {
+//                    BookController.showMenu(); // Kan vara en annan meny
+//                    break;
+//                }
+//                case 2: {
+//                    LoanController.showMenu();
+//                    break;
+//                }
+//                case 3: {
+//                    MemberController.showMenu();
+//                    break;
+//                }
+//                default:
+//                    System.out.println("Invalid Input");
+//            }
+//        }
+//        System.out.println(ANSI.CLEAR_SCREEN);
+//        System.out.println("Exiting application...");
+//        System.exit(0);
+//    }
+//}
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
-
-        // skapa meny som visar information och läser input
         Menu mainMenu = new Menu();
 
-        // Välj vad det ska stå i menytexten med följande metoder:
         mainMenu.setTopTitle("Main Menu");
-        mainMenu.setMidTitle("Subtitle");
-        mainMenu.setMenuInfo("Menu Info");
+        mainMenu.setMidTitle("Choose role");
+        mainMenu.setMenuInfo("Select which menu to open");
         mainMenu.setExitOption("Exit");
-        mainMenu.addMenuOption("Show Book Menu");
-        mainMenu.addMenuOption("Show Loan Menu");
-        mainMenu.addMenuOption("Show Member Menu");
+        mainMenu.addMenuOption("Reader");
+        mainMenu.addMenuOption("Librarian");
         mainMenu.setPrePrompt("Type a number and press enter...");
         mainMenu.setPromptLine("Enter: ");
 
-
-        // Visa menyn genom showMenu() som returnerar en boolean.
         while (mainMenu.showMenu()) {
-            // Läs in valet med en getter i meny-klassen.
-            // Menyn läser bara input. Logiken måste hanteras med en switch.
+            ;
+
             switch (mainMenu.getChoice()) {
-                case 0: {
-                    break; // Avslutar while-loopen (lämnar menyn)
+                case 0 -> {
+                    System.out.println(ANSI.CLEAR_SCREEN);
+                    System.out.println("Exiting application...");
+                    return;
                 }
-                case 1: {
-                    BookController.showMenu(); // Kan vara en annan meny
-                    break;
-                }
-                case 2: {
-                    LoanController.showMenu();
-                    break;
-                }
-                case 3: {
-                    MemberController.showMenu();
-                    break;
-                }
-                default:
-                    System.out.println("Invalid Input");
+                case 1 -> ReaderMenuController.showMenu();
+                case 2 -> LibrarianMenuController.showMenu();
+                default -> System.out.println("Invalid input");
             }
         }
-        System.out.println(ANSI.CLEAR_SCREEN);
-        System.out.println("Exiting application...");
-        System.exit(0);
     }
 }
