848c741a0e111f8cfa04a59f085b7540b3bff34b
nils-fjp <nils.fj.paulsson@outlook.com>
2026-04-01 16:58:28 +0200

    create packages for each controller type and fix minor changes

--- src/Member/LibrarianMenuController.java
+++ src/Member/LibrarianMenuController.java
@@ -1,10 +1,11 @@
+package Member;
 
+import UI.Menu;
 
-
-    public class LibrarianMenuController {
+public class LibrarianMenuController {
         public static void showMenu() {
             Menu menu = new Menu();
-            menu.setTopTitle("Librarian Menu");
+            menu.setTopTitle("Librarian UI.Menu");
             menu.addMenuOption("Dashboard");
             menu.addMenuOption("Books");
             menu.addMenuOption("Borrowers");
@@ -13,15 +14,14 @@
             menu.addMenuOption("Categories");
             menu.addMenuOption("Statistics");
 
-            while (true) {
-                menu.showMenu();
+            while (menu.showMenu()) {
 
                 switch (menu.getChoice()) {
                     case 0 -> { return; }
                     //case 1 -> DashboardController.showDashboard();
-                    //case 2 -> BookController.showLibrarianMenu();
+                    //case 2 -> Book.BookController.showLibrarianMenu();
                     case 3 -> MemberController.showLibrarianMenu();
-                    //case 4 -> LoanController.showAdminLoanMenu();
+                    //case 4 -> Loan.LoanController.showAdminLoanMenu();
                     //case 5 -> AuthorController.showMenu();
                    // case 6 -> CategoryController.showMenu();
                    // case 7 -> StatisticsController.showMenu();
