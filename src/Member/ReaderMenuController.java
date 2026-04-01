df86cc0f6c9ed81bc90198e5070e81d11c2ecd1f
nils-fjp <nils.fj.paulsson@outlook.com>
2026-04-01 16:58:28 +0200

    create packages for each controller type and fix minor changes

--- src/Member/ReaderMenuController.java
+++ src/Member/ReaderMenuController.java
@@ -1,24 +1,27 @@
+package Member;
+
+import UI.Menu;
+
 public class ReaderMenuController {
     public static void showMenu() {
         Menu menu = new Menu();
 
-        menu.setTopTitle("Reader Menu");
+        menu.setTopTitle("Reader UI.Menu");
         menu.setMidTitle("Reader");
         menu.setMenuInfo("Choose section");
         menu.setExitOption("Back");
-        menu.addMenuOption("Library"); //BookRepository
-        menu.addMenuOption("My Loans"); //LoanRepository
-        menu.addMenuOption("My Profile +"); //MemberRepository
+        menu.addMenuOption("Library"); //Book.BookRepository
+        menu.addMenuOption("My Loans"); //Loan.LoanRepository
+        menu.addMenuOption("My Profile +"); //Member.MemberRepository
         menu.setPrePrompt("Type a number and press enter...");
         menu.setPromptLine("Enter: ");
 
-        while (true) {
-            menu.showMenu();
+        while (menu.showMenu()) {
 
             switch (menu.getChoice()) {
                 case 0 -> { return; }
-                //case 1 -> BookController.showReaderMenu();
-                //case 2 -> LoanController.showMenu();
+                //case 1 -> Book.BookController.showReaderMenu();
+                //case 2 -> Loan.LoanController.showMenu();
                 case 3 -> MemberController.showReaderMenu();
                 default -> System.out.println("Invalid input");
             }
