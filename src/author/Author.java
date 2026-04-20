cc66ae5dec460af602ddcd399ffed680d1bdfc48
Olena Vitkovska <olena.vitkovska86@gmail.com>
2026-04-20 13:48:52 +0200

    implement complete author module with all methods

--- src/author/Author.java
+++ src/author/Author.java
@@ -1,20 +1,31 @@
 package author;
 
+import java.time.LocalDate;
+
 public class Author {
+    private Integer id;
     private String first_name;
     private String last_name;
     private String nationality;
-    private String birth_date;
+    private LocalDate birth_date;
 
     public Author() {
-
     }
 
-    public Author(String first_name, String last_name, String nationality, String birth_date) {
+    public Author(Integer id, String first_name, String last_name, String nationality, LocalDate birth_date) {
+        this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
         this.nationality = nationality;
         this.birth_date = birth_date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getFirst_name() {
@@ -25,14 +36,6 @@ public class Author {
         this.first_name = first_name;
     }
 
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
     public String getLast_name() {
         return last_name;
     }
@@ -41,11 +44,19 @@ public class Author {
         this.last_name = last_name;
     }
 
-    public String getBirth_date() {
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public LocalDate getBirth_date() {
         return birth_date;
     }
 
-    public void setBirth_date(String birth_date) {
+    public void setBirth_date(LocalDate birth_date) {
         this.birth_date = birth_date;
     }
 }
