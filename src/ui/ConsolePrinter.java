a61de643874eb053aab6c4d81e5e851fbcb85b0b
nils-fjp <nils.fj.paulsson@proton.me>
2026-04-21 16:40:48 +0200

    center fields header title and add private constants

--- src/ui/ConsolePrinter.java
+++ src/ui/ConsolePrinter.java
@@ -6,20 +6,40 @@ import java.util.Map;
 //import static member.MemberController.printer;
 
 public class ConsolePrinter {
+    private static final int BORDER = 48;
+    private static final int PADDING = BORDER - 4;
 
     public static void printHeader(String title) {
         System.out.println();
-        System.out.println("\t" + ANSI.CYAN + ANSI.BOLD + title + ANSI.NO_BOLD + ANSI.DEFAULT_FG);
-        System.out.println("\t" + ANSI.BRIGHT_BLACK + "────────────────────────────────────────" + ANSI.DEFAULT_FG);
+        String centerFlex = " ".repeat((BORDER - title.length()) / 2);
+        String flexRest = " ".repeat((BORDER - title.length()) % 2);
+        System.out.println("\t" + ANSI.CYAN + ANSI.BOLD + centerFlex + title + flexRest + ANSI.NO_BOLD + ANSI.DEFAULT_FG);
+        System.out.println("\t" + ANSI.BRIGHT_BLACK + "─".repeat(BORDER) + ANSI.DEFAULT_FG);
     }
 
     public static void printFooter() {
-        System.out.println("\t" + ANSI.BRIGHT_BLACK + "────────────────────────────────────────" + ANSI.DEFAULT_FG);
+        System.out.println("\t" + ANSI.BRIGHT_BLACK + "─".repeat(BORDER) + ANSI.DEFAULT_FG);
         System.out.println();
     }
 
     public static void printField(String label, Object value) {
-        System.out.println("\t" + ANSI.YELLOW + label + ": " + ANSI.DEFAULT_FG + value);
+        String centerFlex = " ".repeat((BORDER - label.length()) / 2);
+        System.out.println("\t" + ANSI.YELLOW + label + ": " + centerFlex + ANSI.DEFAULT_FG + value);
+    }
+
+    public static void printPrompt(String message) {
+        String centerFlex = " ".repeat((BORDER - message.length()) / 2);
+        System.out.println("\t" + ANSI.YELLOW + message + centerFlex + ANSI.DEFAULT_FG);
+    }
+
+    public static void printFields(String title, LinkedHashMap<String, Object> fields) {
+        printHeader(title);
+
+        for (Map.Entry<String, Object> entry : fields.entrySet()) {
+            printField(entry.getKey(), entry.getValue());
+        }
+
+        printFooter();
     }
 
     public void printError(String message) {
@@ -33,20 +53,5 @@ public class ConsolePrinter {
         System.out.println("\t" + ANSI.BRIGHT_GREEN + message + ANSI.DEFAULT_FG);
         System.out.println();
     }
-
-    public static void printPrompt(String message) {
-        System.out.println("\t" + ANSI.YELLOW + message + ANSI.DEFAULT_FG);
-    }
-
-    public static void printFields(String title, LinkedHashMap<String, Object> fields) {
-        printHeader(title);
-
-        for (Map.Entry<String, Object> entry : fields.entrySet()) {
-            printField(entry.getKey(), entry.getValue());
-        }
-
-        printFooter();
-    }
-
 
 }
